ec39862b005a29f628f595d287dcd8fe8fca2144
devbuckylabs <dev.buckylabs@gmail.com>
2019-10-08 09:45:12 +0530

    6. Introduction to Node.js and NPM -completed

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,8 @@
+let desc = "Avacado is shit";
+console.log(desc);
+const fs = require('fs');
+const input = fs.readFileSync('./txt/input.txt','UTF-8');
+console.log(input);
+desc = `${desc} \n${input}`;
+fs.writeFileSync('./txt/output.txt', desc);
+console.log('File written');
