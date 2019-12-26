a1d2677850cea7e0b1f93c0efd65674469d252c5
devbuckylabs <dev.buckylabs@gmail.com>
2019-12-26 21:55:24 +0530

    Node-farm Completed

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,8 +1,8 @@
-const fs = require("fs");
-const http = require("http");
-const url = require("url");
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
-const replacePlaceHolder = require("./modules/replaceTemplate");
+const replacePlaceHolder = require('./modules/replaceTemplate');
 
 //****************************/
 //FILES SYNC/
@@ -35,46 +35,46 @@ const replacePlaceHolder = require("./modules/replaceTemplate");
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
-  "UTF-8"
+  'UTF-8'
 );
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
-  "UTF-8"
+  'UTF-8'
 );
 const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
-  "UTF-8"
+  'UTF-8'
 );
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "UTF-8");
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'UTF-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
 
-  if (pathname === "/" || pathname === "/overview") {
+  if (pathname === '/' || pathname === '/overview') {
     const cardshtml = dataObj
       .map(el => replacePlaceHolder(tempCard, el))
-      .join("");
+      .join('');
 
-    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardshtml);
-    res.writeHead(200, { "Content-type": "text/html" });
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardshtml);
+    res.writeHead(200, { 'Content-type': 'text/html' });
     res.end(output);
-  } else if (pathname === "/product") {
-    res.writeHead(200, { "Content-type": "text/html" });
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
     const product = dataObj[query.id];
     const output = replacePlaceHolder(tempProduct, product);
     res.end(output);
-  } else if (pathname === "/api") {
+  } else if (pathname === '/api') {
     res.end(data);
   } else {
     res.writeHead(404, {
-      "Content-type": "text/html",
-      "my-own-header": "Holaa"
+      'Content-type': 'text/html',
+      'my-own-header': 'Holaa'
     });
-    res.end("<h1>404 NOT FOUND</h1>");
+    res.end('<h1>404 NOT FOUND</h1>');
   }
 });
 
-server.listen(8000, "127.0.0.1", () => {
-  console.log("Listening to requests on port 8000");
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
 });
